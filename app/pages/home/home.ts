834720fee43ac4ee9287d5f7f82e1a66f8cdfbae
NVS Abhilash <nvs232@gmail.com>
2016-09-16 20:22:59 +0530

    Added ionic cloud integration

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component }      from '@angular/core';
+import { Auth, User }     from '@ionic/cloud-angular';
+import { NavController }  from 'ionic-angular';
+
+
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  constructor(public auth: Auth, public user: User, public navCtrl: NavController) {
 
   }
 }
